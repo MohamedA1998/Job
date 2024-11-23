5a8f1c97469e9b46f1589eb0cfad1d447afe06c2
Mo7amed <malsayed493@gmail.com>
2024-11-23 14:17:12 +0200

    handel Exception error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
 class Handler extends ExceptionHandler
 {
     /**
@@ -44,5 +44,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $exception) 
+    { 
+        if ($exception instanceof ModelNotFoundException) {
+            return redirect('/');
+        }
+
+        return parent::render($request, $exception);
     }
 }
