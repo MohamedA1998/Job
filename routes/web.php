839ac5f8277264741c32b6fd8fbce6604eb0f125
Mo7amed <mohamed.alsayed@cems-it.com>
2025-02-23 21:41:43 +0200

    ss

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,3 @@ Route::get('/', fn() => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)
     ->only('index', 'show');
-
-Route::resource('icon', JobController::class)
-    ->only('index', 'show');
